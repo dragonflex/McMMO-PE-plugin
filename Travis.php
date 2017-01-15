2bee6d5762b56992df0128910317b47fce48de6b
wsngamerz <wneild5@gmail.com>
2017-01-15 08:10:54 +0000

    try to fix

--- Travis.php
+++ Travis.php
@@ -4,7 +4,7 @@ $server = proc_open(PHP_BINARY.' src/pocketmine/PocketMine.php --no-wizard --dis
     1 => ['pipe', 'w'],
     2 => ['pipe', 'w'],
 ], $pipes);
-fwrite($pipes[0], "version\nmakeplugin McMMO-PE-plugin\nstop\n\n");
+fwrite($pipes[0], "version\nmakeplugin mcmmo-PE\nstop\n\n");
 while(!feof($pipes[1])){
     echo fgets($pipes[1]);
 }
@@ -12,12 +12,12 @@ fclose($pipes[0]);
 fclose($pipes[1]);
 fclose($pipes[2]);
 echo "\n\nReturn value: ".proc_close($server)."\n";
-if(count(glob('plugins/DevTools/McMMO-PE-plugin*.phar')) === 0){
-    echo "Failed to create a McMMO-PE-plugin phar!\n";
+if(count(glob('plugins/DevTools/mcmmo-PE*.phar')) === 0){
+    echo "Failed to create a mcmmo-PE phar!\n";
     exit(1);
 }else{
-    $fn = glob('plugins/DevTools/McMMO-PE-plugin*');
-    rename($fn[0], 'plugins/DevTools/McMMO-PE-plugin.phar');
-    echo "A McMMO-PE-plugin phar was created!\n";
+    $fn = glob('plugins/DevTools/mcmmo-PE*');
+    rename($fn[0], 'plugins/DevTools/mcmmo-PE.phar');
+    echo "A mcmmo-PE phar was created!\n";
     exit(0);
 }
